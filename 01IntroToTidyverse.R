ab08a119b96d77728efbe4d3682c107bccc0e561
RoseMcc04 <mccormacka@appstate.edu>
2024-08-23 13:19:38 -0400

    renamed file so we can have topics in numerical order (only using main branch)

---
+++ 01IntroToTidyverse.R
@@ -0,0 +1,14 @@
+#' Introduction to the Tidyverse Notes
+#' 
+#' @author Rose McCormack
+#' @note 24 August 2024
+#' @description This document will contain notes for a lesson on tidyverse.
+#' 
+
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Load gapminder dataset
+gapminder
+
